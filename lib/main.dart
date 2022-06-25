7c82ff037f8f15d6bd368d21731e0f3c648aec62
Syarif Dairani <syarif.dairani@rga.com>
2022-06-26 03:24:53 +0800

    Dark theme to make the status bar texts visible

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
